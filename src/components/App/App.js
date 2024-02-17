12cc7f2509f7ae799d0ee71ede2aaad5966f56b4
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-02-17 09:21:08 -0800

    Refactor and clean up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,18 @@ import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 import styles from "./App.module.css";
 
-const crosshairSize                 = 100;
-const isAnimated                    = true;
-const globalSpeed                   = 0.01;
-const string                        = "XY";
-const characters                    = string.split("");
-const defaultCharacterYCoordinates  = new Array(characters.length).fill(0);
-const localSpeeds               
-    = defaultCharacterYCoordinates.map(() => Math.random() * globalSpeed);
+function initializeLocalSpeeds (stringLength, globalSpeed) {
+    return new Array(stringLength)
+        .fill(null)
+        .map(() => Math.random() * globalSpeed);
+}
+
+const string        = "XYZ";
+const globalSpeed   = 0.01;
+const crosshairSize = 100;
+const isAnimated    = true;
+const characters    = string.split("");
+const localSpeeds   = initializeLocalSpeeds(string.length, globalSpeed);
 
 function App () {
     const charactersRef = useRef([]);
@@ -40,23 +44,31 @@ function App () {
 
     const { mouseX, mouseY } = useMouseCoordinates();  
     const { height /*, width*/ } = useWindowDimensions();
+    
     //const normalizedMouseX = 100 * mouseX / width;
     //const normalizedMouseY = 100 * mouseY / height;  
+
+    const crosshairY           = mouseY - (crosshairSize / 2);
+    const normalizedCrosshairY = 100 * crosshairY / height; 
 
     useEffect(() => {
         if (isAnimated) {
             const interval = setInterval(() => {
-                const newCharacterBoundingBoxes = characterBoundingBoxes.map((characterBoundingBox, index) => {
-                    if (Math.round(characterBoundingBox.y) === Math.round(100 * (mouseY - (crosshairSize / 2)) / height)) {
-                        //console.log('foo');
-                    }
-                    const newBoundingBoxY = characterBoundingBox.y >= 100
-                        ? (characterBoundingBox.y % 100) - 3
-                        : characterBoundingBox.y + localSpeeds[index];
-                    return { ...characterBoundingBox, y: newBoundingBoxY };                    
-                });
-                setCharacterBoundingBoxes(newCharacterBoundingBoxes);
-            }, 1);
+                const newCharacterBoundingBoxes = characterBoundingBoxes
+                    .map((characterBoundingBox, index) => {
+                        if (
+                                Math.round(characterBoundingBox.y) 
+                            === Math.round(normalizedCrosshairY)
+                        ) {
+                            //console.log('foo');
+                        }
+                        const newBoundingBoxY = characterBoundingBox.y >= 100
+                            ? (characterBoundingBox.y % 100) - 3
+                            : characterBoundingBox.y + localSpeeds[index];
+                        return { ...characterBoundingBox, y: newBoundingBoxY };                    
+                    });
+                    setCharacterBoundingBoxes(newCharacterBoundingBoxes);
+                }, 1);
             return () => clearInterval(interval);
         }
     });
