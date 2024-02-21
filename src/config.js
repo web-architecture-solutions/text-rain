2b0562866ac7cc95cc3d1eee844b501db00ca2d8
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-02-20 19:44:56 -0800

    Polish UI

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const isAnimated      = true;
 
-export const maxSpeed        =  1.62  // Default:  1.62
+export const maxSpeed        =  1.62; // Default:  1.62
 export const minSpeed        =  0.38; // Default:  0.38
 export const framesPerSecond = 66.67; // Default: 66.67
 export const distanceEpsilon =  0.01; // Default:  0.01
