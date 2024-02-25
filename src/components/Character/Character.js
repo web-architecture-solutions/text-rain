5b0fc24a464e3b92183b48c848f2cd1aa6890514
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-02-25 10:01:51 -0800

    Shade characters by mass

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,14 +1,19 @@
 import { forwardRef } from "react";
 
+import { maxMass } from "../../config";
+
 import styles from "./Character.module.css";
 
 const Character = forwardRef(({ value, top, mass, isStopped }, ref) => {
+    const RBGValue = 255 -  (255 * mass / maxMass);
+
     return (
         <span 
             className = {styles.Character}
             style     = {{ 
                 filter: `blur(${isStopped ? 0 : mass}px)`,
-                top: `${top}%` 
+                top: `${top}%`,
+                color:  `rgb(${[RBGValue, RBGValue, RBGValue]})`
             }}
             ref       = {ref}
         >
